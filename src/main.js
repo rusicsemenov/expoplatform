1fb6b9dfcf5087dbb2fb35d1ad8767b12c838e2a
Rusic <rusic@operamail.com>
2019-11-29 20:36:16 +0300

    add axio and firebase

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "./plugins/firebase";
 
 Vue.config.productionTip = false;
+
+window.axios = require("axios");
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+firebase.auth().onAuthStateChanged(function(user) {
+  console.log(user);
+});
 
 new Vue({
   router,
---
+++ src/plugins/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1x0m5B6H7z4xQFr6X7L3q2xnAuBslllA",
+  authDomain: "vialan-expoplatform.firebaseapp.com",
+  databaseURL: "https://vialan-expoplatform.firebaseio.com",
+  projectId: "vialan-expoplatform",
+  storageBucket: "vialan-expoplatform.appspot.com",
+  messagingSenderId: "760182459729",
+  appId: "1:760182459729:web:7ffe70226c278b9da1d770",
+  measurementId: "G-MDYCEQCMEC"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
